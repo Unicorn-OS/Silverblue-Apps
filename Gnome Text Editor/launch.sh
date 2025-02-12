39e3a671062ace36f63d40ccd37e4e2d8b16963d
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-12 02:44:30 -0600

    Create launch.sh

---
+++ Gnome Text Editor/launch.sh
@@ -0,0 +1,3 @@
+# https://www.reddit.com/r/Fedora/comments/1447byn/is_there_a_way_to_launch_the_gnometexteditor_from/
+# https://www.google.com/search?q=silverblue+gnome-text-editor
+flatpak run org.gnome.TextEditor
